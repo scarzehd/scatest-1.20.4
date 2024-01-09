4b06480b058c335d6766e76deadc034006e2fdfc
S. C. Arzehd <scarzehd@gmail.com>
2024-01-08 23:04:45 -0500

    Added support for manual language file additions

--- src/main/java/com/scarzehd/scatest/datagen/EnglishLangaugeProvider.java
+++ src/main/java/com/scarzehd/scatest/datagen/EnglishLangaugeProvider.java
@@ -1,8 +1,11 @@
 package com.scarzehd.scatest.datagen;
 
+import com.scarzehd.scatest.SCATest;
 import com.scarzehd.scatest.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+
+import java.nio.file.Path;
 
 public class EnglishLangaugeProvider extends FabricLanguageProvider {
     protected EnglishLangaugeProvider(FabricDataOutput dataOutput) {
@@ -13,5 +16,12 @@ public class EnglishLangaugeProvider extends FabricLanguageProvider {
     public void generateTranslations(TranslationBuilder translationBuilder) {
         translationBuilder.add(ModItems.RAW_MACARONI, "Raw Macaroni");
         translationBuilder.add(ModItems.BOWL_OF_RAW_MACARONI, "Bowl of Raw Macaroni");
+
+        try {
+            Path existingFilePath = dataOutput.getModContainer().findPath("assets/" + SCATest.MOD_ID + "/lang/en_us.existing.json").get();
+            translationBuilder.add(existingFilePath);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to add existing language file!", e);
+        }
     }
 }
